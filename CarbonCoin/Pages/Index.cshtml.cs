528518de700bdd4a304836dbd3794bd54e9bbacd
krsh2001 <dynosa,ead@gmail.com>
2020-01-25 17:17:39 -0500

    Added basic HTTP functionality

--- CarbonCoin/Pages/Index.cshtml.cs
+++ CarbonCoin/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,15 +12,22 @@ namespace CarbonCoin.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IHttpClientFactory _clientFactory;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory clientFactory)
         {
             _logger = logger;
+            _clientFactory = clientFactory;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
+            /*var request = new HttpRequestMessage(HttpMethod.Get, "url...");
+            request.Headers.Add("Top Stats", "");
 
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);*/
         }
     }
 }
--- CarbonCoin/Startup.cs
+++ CarbonCoin/Startup.cs
@@ -24,6 +24,7 @@ namespace CarbonCoin
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
