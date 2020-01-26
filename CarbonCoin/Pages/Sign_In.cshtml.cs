605d8d884ca28a572e722f327ccef3bb63a0ee9d
krsh2001 <dynosa,ead@gmail.com>
2020-01-25 20:04:57 -0500

    fixed button bug issues

--- CarbonCoin/Pages/Sign_In.cshtml.cs
+++ CarbonCoin/Pages/Sign_In.cshtml.cs
@@ -10,25 +10,32 @@ namespace CarbonCoin
 {
     public class Sign_InModel : PageModel
     {
-        private string username, password;
         private readonly IHttpClientFactory _clientFactory;
 
-        public Sign_InModel(IHttpClientFactory clientFactory) {
-            _clientFactory = clientFactory;
-        }
+        [BindProperty]
+        public string username { get; set; }
+
+        [BindProperty]
+        public string password { get; set; }
 
         public void OnGet()
         {
+            // you can initialize the values. for example I set the username
+            username = "test";
         }
 
-        public void submitButtonClick(object sender, EventArgs e) {
-            username = String.Format("{0}", Request.Form["username"]);
-            password = String.Format("{0}", Request.Form["password"]);
+        public IActionResult OnPost()
+        {
+            // do something with username and password
 
-            //var request = new HttpRequestMessage(HttpMethod.Get, "");
-            //request.Headers.Add("Username", "");
+            if (string.IsNullOrEmpty(password))
+            {
+                ModelState.AddModelError("password", "Password is a required field.");
+                return Page();
+            }
 
-            Response.Redirect("~/Index");
+            // or you can redirect to another page
+            return RedirectToPage("./Index");
         }
     }
 }
