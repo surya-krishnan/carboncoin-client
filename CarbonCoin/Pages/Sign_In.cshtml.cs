2535507722a91e9e64eccc504800e7b952e47f4b
krsh2001 <dynosa,ead@gmail.com>
2020-01-26 00:24:14 -0500

    fixed networking bugs

--- CarbonCoin/Pages/Sign_In.cshtml.cs
+++ CarbonCoin/Pages/Sign_In.cshtml.cs
@@ -5,12 +5,16 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+using System.IO;
+using System.Text;
 
 namespace CarbonCoin
 {
     public class Sign_InModel : PageModel
     {
-        private readonly IHttpClientFactory _clientFactory;
+
+        private bool success;
 
         [BindProperty]
         public string username { get; set; }
@@ -18,12 +22,18 @@ namespace CarbonCoin
         [BindProperty]
         public string password { get; set; }
 
+        private readonly IHttpClientFactory _clientFactory;
+
+        public Sign_InModel(IHttpClientFactory clientFactory) {
+            _clientFactory = clientFactory;
+        }
+
         public void OnGet()
         {
 
         }
 
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
             // do something with username and password
 
@@ -43,10 +53,31 @@ namespace CarbonCoin
             {
                 ModelState.AddModelError("password", "Password is a required field.");
                 return Page();
-            } 
+            }
 
-            // or you can redirect to another page
-            return RedirectToPage("./Index");
+
+            //Uri siteUri = new Uri("172.17.79.129:3000/auth/user/philnic");
+            //string bob = Get(siteUri);
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://172.17.79.129:3000/auth/user/philnic");
+            request.Content = new StringContent("{\"pass\":\"000\"}", Encoding.UTF8, "application/json");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToPage("./Index");
+            }
+            else
+            {
+                return RedirectToPage("./MyAccount");
+            }
+
+
         }
+
+       
     }
 }
--- CarbonCoin/Startup.cs
+++ CarbonCoin/Startup.cs
@@ -23,8 +23,8 @@ namespace CarbonCoin
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
             services.AddHttpClient();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
