9dd0b135c8ee0ee8729aefc825828a3049999b6d
krsh2001 <dynosa,ead@gmail.com>
2020-01-26 13:00:54 -0500

    Major changes

--- CarbonCoin/Pages/MyAccount.cshtml.cs
+++ CarbonCoin/Pages/MyAccount.cshtml.cs
@@ -1,6 +1,10 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using System.Web;
+using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
 
@@ -14,13 +18,20 @@ namespace CarbonCoin
 
     }
 
+    public class TransactionData {
+        public string senderName { get; set; }
+        public string recipientName { get; set; }
+        public double cashTransfer { get; set; }
+        public double carbonTransfer { get; set; }
+    }
+
     public class MyAccountModel : PageModel
     {
 
         private readonly IHttpClientFactory _clientFactory;
         public BalanceInfo info;
-        public string ye { get; private set; } = "lololol";
-        public string responseStr { get; private set; } = "xx";
+        public List<TransactionData> transData;
+        public string htmlStr = "";
 
         public MyAccountModel(IHttpClientFactory clientFactory) {
             _clientFactory = clientFactory;
@@ -41,16 +52,20 @@ namespace CarbonCoin
 
                 info = JsonConvert.DeserializeObject<BalanceInfo>(json);
 
-                string name, carbonTransfer, cashTransfer, timeStamp;
-
-                request = new HttpRequestMessage(HttpMethod.Get, "");
+                request = new HttpRequestMessage(HttpMethod.Get, "http://172.17.79.129:3000/transactions/viewable");
                 request.Headers.Add("auth", Constants.token);
 
-                
+                response = await client.SendAsync(request);
+
+                if (response.IsSuccessStatusCode) {
+                    json = await response.Content.ReadAsStringAsync();
+
+                    transData = JsonConvert.DeserializeObject<List<TransactionData>>(json);
+  
+                }
 
 
             }
         }
-
     }
 }
--- CarbonCoin/Pages/Shop.cshtml.cs
+++ CarbonCoin/Pages/Shop.cshtml.cs
@@ -1,16 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace CarbonCoin.Pages
 {
+    public class StoreItem {
+        public string name { get; set; }
+        public double cashCost { get; set; }
+        public double carbonCost { get; set; }
+        public int leftInStock { get; set; }
+    }
+
     public class ShopModel : PageModel
     {
-        public void OnGet()
+        IHttpClientFactory _clientFactory;
+        public List<StoreItem> storeItems;
+
+        public ShopModel(IHttpClientFactory clientFactory)
         {
+            _clientFactory = clientFactory;
+        }
+
+        public async void OnGet()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://172.17.79.129:3000/store/inventory");
+            request.Headers.Add("auth", Constants.token);
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+
+                storeItems = JsonConvert.DeserializeObject<List<StoreItem>>(json);
+            }
         }
     }
 }
